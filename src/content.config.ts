4f831a6363d19cb9b96bd089ded2973c38f484e4
Martin Hooper <martin@mjhooper.co.uk>
2025-02-15 20:08:37 +0000

    .

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from "astro:content";
-import { glob, file } from "astro/loaders";
+import { file } from "astro/loaders";
 
 const data = defineCollection({
     loader: file("src/data/product.json"),
