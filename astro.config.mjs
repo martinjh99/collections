3528b13ba0a68108eca5a3b04bb04cdda2ebe11c
Martin Hooper <martin@mjhooper.co.uk>
2025-03-02 08:30:00 +0000

    Changed astro settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+   build: {
+      format: 'file',
+   },
 vite:{
    plugins: [tailwindcss()],
 },
